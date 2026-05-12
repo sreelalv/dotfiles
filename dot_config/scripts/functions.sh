d4dfd26e32cbb0f45f9f6ed69f7394de2156e24a
sreelalv <sreelalv.sr@gmail.com>
2026-05-12 13:13:02 +0530

    Update tt() for tmux

--- dot_config/scripts/functions.sh
+++ dot_config/scripts/functions.sh
@@ -17,25 +17,23 @@ quiet(){
 
 tt(){
 	if [[ "$#" == 0 ]] ; then 
-		tmux ls 
+		tmux
 		return 
 	fi
 	local arg="$1"
-	local list=( $(tmux ls | awk -F':' '{print $1}') )
+	local list=( $(tmux ls 2>/dev/null| awk -F':' '{print $1}' ) )
 	for i in "${list[@]}" ; do 
 		if [[ "$arg" = "$i" ]] ;then 
 			tmux attach -t "$i" 
 			return
 		fi
 	done
-	if [[ "$1" =~ ^[Aa]([Tt]([Tt]([Aa]([Cc]([Hh]?)?)?)?)?)? ]]  ; then 
+	if [[ "$1" =~ "ls" ]] ; then
+		tmux ls
+	elif [[ "$1" =~ ^[Aa]([Tt]([Tt]([Aa]([Cc]([Hh]?)?)?)?)?)? || "$1" =~ ^[Aa]$ ]]  ; then 
 		tmux attach 
 	else
-		read -p "Create Tmux session $arg. Type [Yes] to continue --> " -n 4 val;
-
-		if [[ $val =~ ^[Yy][Ee][Ss]* ]]; then
-			tmux new -s "$arg"
-		fi 
+		tmux new -s "$arg"
 	fi
 }
 
