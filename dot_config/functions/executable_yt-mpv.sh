681623f9d10eb7ac6553dc5965056a3c4f0a0f93
sreelalv <sreelalv.sr@gmail.com>
2026-09-04 23:30:38 +0530

    yt-mpv playback setup in i3

--- dot_config/functions/executable_yt-mpv.sh
+++ dot_config/functions/executable_yt-mpv.sh
@@ -1,15 +1,14 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
 
-yt-mpv(){
-    local mpv_entry="$(zenity --forms --title='mpv' --text='' --add-entry='Paste the link' --add-entry='Name')"
-    IFS='|' read -r url comment <<< $mpv_entry
-    if ! [[ -n "url" ]] ; then 
-        return
-    else
-        if [[ -n "$comment" ]]; then 
-            printf "%s|%s\n" "$comment" "$url" >> $HOME/.config/mpv/mpv_history
-        fi
-        mpv "$url"
-    fi
+yt-mpv() {
+  local mpv_entry="$(zenity --forms --title='mpv' --text='' --add-entry='Paste the link' --add-entry='Name')"
+  IFS='|' read -r url comment <<<$mpv_entry
+  if ! [[ -z "$url" ]]; then
+    return
+  fi
+  if [[ -n "$comment" ]]; then
+    printf "%s|%s\n" "$comment" "$url" >>$HOME/.config/mpv/mpv_history
+  fi
+  mpv "$url"
 }
 yt-mpv
